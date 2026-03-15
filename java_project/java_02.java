55b36b173fd047450455cf9cdc9d4412f0ead33b
却独留琴与笛 <2971628645@QQ.com>
2026-03-15 11:23:42 +0800

    Fix syntax errors and improve bubble sort implementation

--- java_project/java_02.java
+++ java_project/java_02.java
@@ -1,22 +1,29 @@
-import java.util.*
+import java.util.*;
 
-public class java_o2{
-  public static viod main(String arg[]){
+public class java_02 {
+	public static void main(String arg[]){
     Scanner reader = new Scanner(System.in);
+    System.out.println("请输入排序数的个数：");
     int n = reader.nextInt();
     int[] nums = new int[n];
+    System.out.println("请输入要排序的数");
+    int i;
+    for(i = 0, i < n, i++){
+    	nums[i] = reader.nextInt();
+    }
     bubble_sort(nums, n);
-    for(int i = 0; i < n; i++){
-      System.out.print(num[i] + " ");
+    for(i = 0, i < n, i++){
+      System.out.print(nums[i] + " ");
     }
   }
 
-  public void bubble_sort(int[] nums ,int n){
+	public static void bubble_sort(int[] nums ,int n){
     int last;    //当前排序的最后一位
     int sort = n;    //判断是否排序 为 0 时未排序
+    int i;
     while(sort > 0){
       last = 0;
-      for(int i = 1, i < sort, i++){
+      for(i = 1, i < sort, i++){
         if(nums[i-1] > nums[i+1]){
           int temp = nums[i-1];
           nums[i-1] = nums[i];
@@ -29,5 +36,5 @@ public class java_o2{
           break;
       }
     }
-  }      
+  }
 }
