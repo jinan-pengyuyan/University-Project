9c6e8219671cd16d0dd9d253db8e43363d38bd4d
却独留琴与笛 <2971628645@QQ.com>
2026-03-16 22:21:48 +0800

    Refactor longest substring calculation and output

--- java_project/java_04.java
+++ java_project/java_04.java
@@ -7,20 +7,25 @@ public class java_04 {
 		Scanner reader = new Scanner(System.in);
 		System.out.println("请输入要计算的字符串");
 		String str = reader.next();
+		int result = find_longest_subString(str);
+		System.out.println("这段字符串的最长不含重复的字串的长度为：" + result);
+	}
+
+	public static int find_longest_subString(String str) {
 		Set<Character> hasappeal = new HashSet<Character>();
 		int n = str.length();
 		int rk = -1;
 		int result = 0;
-		for(int i = 0; i < n; ++i) {
-			if(i != 0) {
+		for (int i = 0; i < n; ++i) {
+			if (i != 0) {
 				hasappeal.remove(str.charAt(i));
 			}
-			while(rk + 1 < n && !hasappeal.contains(str.charAt(rk + 1))) {
+			while (rk + 1 < n && !hasappeal.contains(str.charAt(rk + 1))) {
 				hasappeal.add(str.charAt(rk + 1));
 				++rk;
 			}
-			result = Math.max(result,rk - i + 1);
+			result = Math.max(result, rk - i + 1);
 		}
-		System.out.println("这段字符串的最长不含重复的字串的长度为：" + result);
+		return result;
 	}
 }
