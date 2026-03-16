e21bc77c8e757d37ca1d89591e8c4f84a9dec761
却独留琴与笛 <2971628645@QQ.com>
2026-03-16 22:15:33 +0800

    Add functionality to find longest unique substring length
    This program calculates the length of the longest substring without repeating characters from user input.

---
+++ java_project/java_04.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class java_04 {
+	public static void main(String arg[]) {
+		Scanner reader = new Scanner(System.in);
+		System.out.println("请输入要计算的字符串");
+		String str = reader.next();
+		Set<Character> hasappeal = new HashSet<Character>();
+		int n = str.length();
+		int rk = -1;
+		int result = 0;
+		for(int i = 0; i < n; ++i) {
+			if(i != 0) {
+				hasappeal.remove(str.charAt(i));
+			}
+			while(rk + 1 < n && !hasappeal.contains(str.charAt(rk + 1))) {
+				hasappeal.add(str.charAt(rk + 1));
+				++rk;
+			}
+			result = Math.max(result,rk - i + 1);
+		}
+		System.out.println("这段字符串的最长不含重复的字串的长度为：" + result);
+	}
+}
